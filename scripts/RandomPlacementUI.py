1705addc3942e21e8bee6f4128f01575e49469b9
Katie Morley <kamytherabbit@gmail.com>
2020-12-01 13:20:38 -0700

    working now

--- scripts/RandomPlacementUI.py
+++ scripts/RandomPlacementUI.py
@@ -9,20 +9,20 @@ class RandomPlacementUI():
         self.delete()
         
         self.RandomPlacement_Window = cmds.window(title = 'RandomePlacement Window Settings', widthHeight=(500,500))
-        self.colLayout = cmds.columnLayout(parent=self.RandomePlacement_Window, adjustableColumn=True )
-        cmds.text(label="Number of Duplicates", parent = self.layout_var)
+        self.colLayout = cmds.columnLayout(parent=self.RandomPlacement_Window, adjustableColumn=True )
+        cmds.text(label="Number of Duplicates", parent = self.colLayout)
         self.numDups = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Minimun X", parent = self.layout_var)
+        cmds.text(label="Minimun X", parent = self.colLayout)
         self.minX = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Maximun X", parent = self.layout_var)
+        cmds.text(label="Maximun X", parent = self.colLayout)
         self.maxX = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Minimun Y", parent = self.layout_var)
+        cmds.text(label="Minimun Y", parent = self.colLayout)
         self.minY = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Maximun Y", parent = self.layout_var)
+        cmds.text(label="Maximun Y", parent = self.colLayout)
         self.maxY = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Minimun Z", parent = self.layout_var)
+        cmds.text(label="Minimun Z", parent = self.colLayout)
         self.minZ = cmds.intField(parent=self.colLayout)
-        cmds.text(label="Maximun Z", parent = self.layout_var)
+        cmds.text(label="Maximun Z", parent = self.colLayout)
         self.maxZ = cmds.intField(parent=self.colLayout)
         cmds.button(parent=self.colLayout, label='Set', command=lambda *x: self.RandomPlacement())
         
@@ -36,25 +36,25 @@ class RandomPlacementUI():
     def RandomPlacement(self):
         
         sel = cmds.ls(selection=True)
-        numberOfDups = cmds.intField(self.numDups, q=True, int=True)
-        min_X = cmds.intField(self.minX, q=True, int=True)
-        max_X = cmds.intField(self.maxX, q=True, int=True)
-        min_Y = cmds.intField(self.minY, q=True, int=True)
-        max_Y = cmds.intField(self.maxY, q=True, int=True)
-        min_Z = cmds.intField(self.maxZ, q=True, int=True)
-        max_Z = cmds.intField(self.maxZ, q=True, int=True)
-    
+        numberOfDups = cmds.intField(self.numDups, q=True, value=True)
+        minX = cmds.intField(self.minX, q=True, value=True)
+        maxX = cmds.intField(self.maxX, q=True, value=True)
+        minY = cmds.intField(self.minY, q=True, value=True)
+        maxY = cmds.intField(self.maxY, q=True, value=True)
+        minZ = cmds.intField(self.maxZ, q=True, value=True)
+        maxZ = cmds.intField(self.maxZ, q=True, value=True)
+
         for obj in range(len(cmds.ls(selection=True))):
             index = obj
-        
-            for obj in range(numDuplicates):
-                tempObj = (cmds.duplicate(sel[index]), rr=True)
-                randomX = random.uniform(min_X, max_X)
-                randomY = random.uniform(min_Y, max_Y)
-                randomZ = random.uniform(min_Z, max_Z)
-            
+
+            for obj in range(numberOfDups):
+                tempObj = (cmds.duplicate(sel[index], rr=True))
+                randomX = random.uniform(minX, maxX)
+                randomY = random.uniform(minY, maxY)
+                randomZ = random.uniform(minZ, maxZ)
+
                 cmds.select(tempObj)
-                cmds.xform(worldSpace=True, translation=[randomX, randomY, randomZ])  
+                cmds.xform(worldSpace=True, translation=[randomX, randomY, randomZ]) 
 
 
 RandomPlacement_Window = RandomPlacementUI()
