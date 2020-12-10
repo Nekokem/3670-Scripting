4daf00c1ee9da4395eda6925164024d4d797ccb6
Katie Morley <kamytherabbit@gmail.com>
2020-12-10 10:27:18 -0700

    minor edits

--- scripts/km_Tools/__init__.py
+++ scripts/km_Tools/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
-if '/Users/Katie/3670-Scripting/scripts' not in sys.path:
+if 'MacintoshHD/Users/Katie/3670-Scripting/scripts/km_Tools' not in sys.path:
     
-   sys.path.append('/Users/Katie/3670-Scripting/scripts')
+   sys.path.append('MacintoshHD/Users/Katie/3670-Scripting/scripts/km_Tools')
    
 reload(sys)
