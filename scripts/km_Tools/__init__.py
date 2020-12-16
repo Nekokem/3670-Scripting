bdcfc8ec080533214a8985f6a902dc1e9515ac36
Katie Morley <kamytherabbit@gmail.com>
2020-12-16 11:05:01 -0700

    added module

--- scripts/km_Tools/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if 'MacintoshHD/Users/Katie/3670-Scripting/scripts/km_Tools' not in sys.path:
-    
-   sys.path.append('MacintoshHD/Users/Katie/3670-Scripting/scripts/km_Tools')
-   
-reload(sys)
