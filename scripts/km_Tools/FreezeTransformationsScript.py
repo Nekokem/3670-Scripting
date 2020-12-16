8a5acbf5682ed9bf742d0bad540abc01327e5de3
Katie Morley <kamytherabbit@gmail.com>
2020-12-16 11:04:41 -0700

    added freeze transforms

--- scripts/km_Tools/FreezeTransformationsScript.py
+++ scripts/km_Tools/FreezeTransformationsScript.py
@@ -1,7 +1,23 @@
 import maya.cmds as cmds
 
-def FreezeTrans():
-    sel = cmds.ls(selection=True)
-    FreezeTransformations
-    preformFreezeTransformations(0)
-    cmds.makeIdentity (apply=True, t=1, r=1, s=1, n=0, pn=1)
+class FreezeTransUI():
+   def __init__(self):
+       self.FreezeWindow = 'FreezeWindowSetting'
+    
+   def create(self):
+       
+       self.delete()
+
+       self.FreezeWindow = cmds.window(title = 'Freeze Window Settings', widthHeight=(500,500))
+       self.colLayout = cmds.columnLayout(parent=self.FreezeWindow, adjustableColumn=True )
+       cmds.button(parent=self.colLayout, label='Freeze', command=lambda *x: self.FreezeTrans())
+
+       cmds.showWindow(self.FreezeWindow)
+    
+   def delete(self):
+       if cmds.window(self.FreezeWindow, exists=True):
+           cmds.deleteUI(self.FreezeWindow)
+            
+   def FreezeTrans(self):
+       self.sel = cmds.ls(selection=True)
+       cmds.makeIdentity (self.sel, apply=True, t=1, r=1, s=1, n=0, pn=1)        
