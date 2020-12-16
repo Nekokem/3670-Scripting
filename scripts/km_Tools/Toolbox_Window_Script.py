faeeb344cf630b6c656927e0e3b9254e982fe363
Katie Morley <kamytherabbit@gmail.com>
2020-12-16 11:02:37 -0700

    finished toolbox window

--- scripts/km_Tools/Toolbox_Window_Script.py
+++ scripts/km_Tools/Toolbox_Window_Script.py
@@ -1,14 +1,72 @@
-import km_Tools
-help(km_Tools)
+import maya.cmds as cmds
 
-ToolBoxWindow = 'ToolBoxWindowSettings'
+class ToolBoxUI():
+    def __init__(self):
+        self.ToolBoxWindow = 'ToolBoxWindowSettings'
 
-Renamer = km_Tools.RenamerWindowScript.RenamerUI()
+    def create(self):
+        
+        self.ToolBoxWindow = cmds.window(title = 'ToolBox Window Settings', widthHeight=(500,500))
+        self.colLayout = cmds.columnLayout(parent=self.ToolBoxWindow, adjustableColumn=True )
+        cmds.button(parent=self.colLayout, label='Renamer', command=lambda *x: self.RenamerButton())
+        cmds.button(parent=self.colLayout, label='Randomize', command=lambda *x: self.RandomButton())
+        cmds.button(parent=self.colLayout, label='Freeze Transforms', command=lambda *x: self.FreezeButton())
+        cmds.button(parent=self.colLayout, label='Delete History', command=lambda *x: self.HistoryDeleteButton())
+        cmds.button(parent=self.colLayout, label='Group', command=lambda *x: self.ParentGrpButton())
+        cmds.button(parent=self.colLayout, label='Constrain', command=lambda *x: self.ConstrainButton())
+        cmds.button(parent=self.colLayout, label='Toggle', command=lambda *x: self.ToggleButton())
 
-ToolBoxWindow = cmds.window(title = 'ToolBox Window Settings', widthHeight=(500,500))
-colLayout = cmds.columnLayout(parent=ToolBoxWindow, adjustableColumn=True )
-cmds.button(parent=self.colLayout, label='Renameer', command='Renamer.create()')
 
-Random = km_Tools.RandomPlacementUI.RandomPlacementUI()
+        cmds.showWindow(self.ToolBoxWindow)
+        
+        
+    def delete(self):
+        if cmds.window(self.ToolBoxWindow, exists=True):
+            cmds.deleteUI(self.ToolBoxWindow)
+            
+    def RenamerButton(self):
+        import RenamerWindowScript
+        reload(RenamerWindowScript)
+        Renamer = RenamerWindowScript.RenamerUI()
+        Renamer.create()
+        
+    def RandomButton(self):
+        import RandomPlacementUI
+        reload(RandomPlacementUI)
+        Random = RandomPlacementUI.RandomPlaceUI()
+        Random.create()
+        
+    def FreezeButton(self):
+        import FreezeTransformationsScript
+        reload(FreezeTransformationsScript)
+        Freeze = FreezeTransformationsScript.FreezeTransUI()
+        Freeze.FreezeTrans()
+        
+    def HistoryDeleteButton(self):
+        import HistoryDelete
+        reload(HistoryDelete)
+        History = HistoryDelete.HistoryDeleteUI()
+        History.HistoryDelete()
+        
+    def ParentGrpButton(self):
+        import ParentGrpScript
+        reload(ParentGrpScript)
+        Grp = ParentGrpScript.ParentGroupUI()
+        Grp.ParentGroupFunc()
+        
+    def ConstrainButton(self):
+        import ConstrainScript
+        reload(ConstrainScript)
+        Constrain = ConstrainScript.ConstrainUI()
+        Constrain.ConstrainFunc()
+        
+    def ToggleButton(self):
+        import ToggleAxesScript
+        reload(ToggleAxesScript)
+        Toggle = ToggleAxesScript.ToggleAxes()
+        Toggle.Toggle()
+            
 
-cmds.button(parent=self.colLayout, label='RandomPlacement', command='Random.create()')
+ToolBoxWindow = ToolBoxUI()
+
+ToolBoxWindow.create()                                      
